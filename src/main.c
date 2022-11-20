478e3dae185b71c2e07db1197d6f3b8302b623d3
Daniel Gross <dgross@1-E-13.42heilbronn.de>
2022-11-20 14:05:04 +0100

    nothing

--- src/main.c
+++ src/main.c
@@ -6,14 +6,27 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 16:05:58 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/16 16:18:37 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/20 14:01:07 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h> // printf
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int	main(void)
+{
+	while (1)
+	{
+		readline("Minishell$ ");
+		test();
+	}
+	return (0);
+}
+
+int test(void)
 {
 	return (0);
 }
