429b8fe730de28bddf004fe2c163cfbc4ce80ca4
Daniel Gross <dgross@1-E-11.42heilbronn.de>
2022-11-21 16:48:25 +0100

    added files

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/12 16:05:58 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/20 14:01:07 by dgross           ###   ########.fr       */
+/*   Created: 2022/11/21 16:43:44 by dgross            #+#    #+#             */
+/*   Updated: 2022/11/21 16:43:45 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-#include <stdio.h> // printf
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
-int	main(void)
-{
-	while (1)
-	{
-		readline("Minishell$ ");
-		test();
-	}
-	return (0);
-}
-
-int test(void)
-{
-	return (0);
-}
