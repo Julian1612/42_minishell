b7b61a4261f4259f6fe3972b7571d8fb1ac7b264
DGross245 <103501922+DGross245@users.noreply.github.com>
2022-11-24 19:35:31 +0100

    commit

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:43:44 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/24 17:35:44 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/24 19:33:49 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,31 @@
 #include <unistd.h> //chdir
 #include <stdlib.h> // getenv
 
-int	ft_cd(t_koopa *shell)
+static update_pwd()
+{
+	
+}
+
+int	ft_cd(t_koopa *shell) // only relative or absolute path 
 {
 	if (shell->argc == 1)
 	{
 		// wenn nur cd gecalled wird
 		if(!chdir(getenv("HOME")))
-			/*error message*/
+		[
+			/*error message*/		
+		]
 	}
 	else
 	{
 		// wenn cd + path gecalled wird
 		if (!chdir(shell->cmd_str[2]))
+		{
+			/*error message*/	
+		}
 			
 	}
 	// wenn alles geklappt hat wird pwd geupdated
+	// entwerder die envp PWD editieren oder irgendwas anderes 
+	update_pwd();
 }
--- src/erro.c
+++ src/erro.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   erro.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 16:43:47 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/24 19:21:27 by dgross           ###   ########.fr       */
+/*   Created: 2022/11/24 19:18:31 by dgross            #+#    #+#             */
+/*   Updated: 2022/11/24 19:19:29 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "libft.h" // ft_stcmp
-#include <stdio.h> // printf
+#include <stdio.h> //perror	
 
-int ft_echo(t_koopa *shell)
+int print_error()
 {
-	int n_flag;
-
-	n_flag = False;
-	if (ft_strcmp(shell->flag, "-n") == True)
-		n_flag = True;
-	printf("%s", shell->str);
-	if (!n_flag)
-		printf("\n");
-	shell->exit_status = 1;
-	return (0);
+	
 }
