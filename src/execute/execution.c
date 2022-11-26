4ceeb319f32183fca98e39c6bbe571b2041c3604
Daniel Gross <dgross@3-D-2.42heilbronn.de>
2022-11-26 15:30:32 +0100

    commit

--- src/execute/execution.c
+++ src/execute/execution.c
@@ -6,13 +6,15 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 10:13:09 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/26 12:18:44 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/26 15:29:01 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <unistd.h> // dup access fork
 
+// main function die für die executio von cmd und builtins zuständig ist
+// hier wird dann auch der exec_status abgefangen und ausgegeben nach execution
 int	ft_execute(t_koopa *shell)
 {
 	int tmp_fd;
--- src/execute/redirection.c
+++ src/execute/redirection.c
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 12:15:09 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/26 13:55:43 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/26 15:24:33 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 void ft_redirection(t_koopa *shell)
 {
-	
+	// redirection ausführen
 }
 
 void check_redirection(t_koopa *shell)
 {
-	
+	// in der cmd line nachgucken ob es redirection gibt und
+	// ob man sie auch öffnen kann und halt erstellen
+	// aber I guess wie in pipex also in ft_redirection
 }
