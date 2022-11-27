61ac68f6e887743039b02ab927030b750d73acc2
DGross245 <103501922+DGross245@users.noreply.github.com>
2022-11-27 22:49:35 +0100

    commit

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,24 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dna <dna@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 14:01:00 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/27 19:19:24 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/27 22:35:47 by dna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdlib.h> // free
 
+int ft_name_len(char *name)
+{
+	int i;
+
+	if (name == NULL)
+		return (-1);
+	i = ft_strchr(name, '=');
+	return (i + 1);
+}
+
 char *ft_getenv(t_koopa *shell, char *name)
 {
 	int i;
 
 	i = -1;
-	while(shell->envp[++i] != NULL)
+	while (shell->envp[++i] != NULL)
 	{
-		if(ft_strncmp(shell->envp[i], name, ft_strlen(name)))
+		if (ft_strncmp(shell->envp[i], name, ft_strlen(name)))
 			return (shell->envp[i]);
 	}
 	return (NULL);
