4e522552532d44b63e80aea6abc05a25b5a2f071
Daniel Gross <dgross@1-E-13.42heilbronn.de>
2022-11-29 14:39:23 +0100

    commit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,15 +6,13 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:43:52 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/24 15:24:54 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/29 13:56:55 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//kills the hole project
-//just freeing everything and then exit
 void	ft_exit(t_koopa *shell)
 {
-	exit(0); // exitstatus hab ich kp ob das immer 0 sein muss oder irgendwie 127 	
+	exit(0);
 }
--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,16 +6,14 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 10:13:09 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/29 10:49:44 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/29 14:19:07 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <unistd.h> // dup access fork
 
-// main function die für die executio von cmd und builtins zuständig ist
-// hier wird dann auch der exec_status abgefangen und ausgegeben nach execution
-int	ft_execute(t_koopa *shell)
+int	ft_execute(t_koopa *shell, t_data *data)
 {
 	int	tmp_fd;
 
