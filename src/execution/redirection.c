aab56e2931b2850f427519e568493a7722d44c95
Daniel Gross <dgross@1-E-13.42heilbronn.de>
2022-11-29 15:35:31 +0100

    commit

--- src/execution/redirection.c
+++ src/execution/redirection.c
@@ -6,22 +6,13 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 12:15:09 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/29 10:50:04 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/29 15:24:36 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.c"
+#include "minishell.h"
 
 void	ft_redirection(t_koopa *shell)
 {
 	// redirection ausführen
 }
-
-void	check_redirection(t_koopa *shell)
-{
-	// in der cmd line nachgucken ob es redirection gibt und
-	// ob man sie auch öffnen kann und halt erstellen
-	// aber I guess wie in pipex also in ft_redirection
-}
-
-// kann man beides auch zusammen machne aber müssen gucken wo wir bzw wann wir genau redirecten und wo wir gucken obs welche gibt
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,16 +6,16 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 14:01:00 by dgross            #+#    #+#             */
-/*   Updated: 2022/11/28 15:29:21 by dgross           ###   ########.fr       */
+/*   Updated: 2022/11/29 15:35:02 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdlib.h> // free
 
-int ft_name_len(char *name)
+int	ft_name_len(char *name)
 {
-	int i;
+	int	i;
 
 	if (name == NULL)
 		return (-1);
@@ -23,9 +23,9 @@ int ft_name_len(char *name)
 	return (i + 1);
 }
 
-char *ft_getenv(t_koopa *shell, char *name)
+char	*ft_getenv(t_koopa *shell, char *name)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (shell->envp[++i] != NULL)
@@ -36,9 +36,9 @@ char *ft_getenv(t_koopa *shell, char *name)
 	return (NULL);
 }
 
-void free_evnp(t_koopa *shell)
+void	free_evnp(t_koopa *shell)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (shell->envp[++i] != NULL)
