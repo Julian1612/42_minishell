80c9899e2a585e1356bfa9e152cd0fc8326326d0
Julian Schneider <jtschneider99@icloud.com>
2022-12-09 15:32:57 +0100

    Tokenizer is working but there are still some edgecases

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -3,45 +3,78 @@
 #include <stdlib.h>
 #include "minishell.h"
 
-void count_str_len(char *str, int *j, int *token_length)
+void counter_dqoute_len(char *str, int *j, int *token_len);
+void counter_quote_len(char *str, int *j, int *token_len);
+
+void count_str_len(char *str, int *j, int *token_len)
 {
 	while ((str[*j] >= '!' && str[*j] <= '~') && str[*j] != '\0')
 	{
 		(*j)++;
-		(*token_length)++;
+		(*token_len)++;
 	}
 }
 
-// Hier wird dann i immer weiter hoch gezählt um durch den string zu gehen
-// quasi um von string zu string zu springen
-// gleihzeitig muss in der function mit token_length gezaehlt werden
-// Aus der differenz von i und token_length kann dann die laenge des tokens
-// berechnet werden anschließend kann dann returned werden
-// und das token erstellt.
+void counter_flag_len(char *str, int *j, int *token_len)
+{
+	(*j)++;
+	(*token_len)++;
+	while (((str[*j] >= 'a' && str[*j] <= 'z')
+			|| (str[*j] >= 'A' && str[*j] <= 'Z'))
+			&&str[*j] != '\0')
+	{
+		if (str[*j + 1] == '"' || str[*j + 1] == 39)
+		{
+			(*j) += 2;
+			while (str[*j] != '"' && str[*j] != '\0')
+			{
+				// printf("str[%d] = %c\n", (*i), str[*i]);
+				(*j)++;
+			}
+			(*j)++;
+		}
+		(*j)++;
+		(*token_len)++;
+	}
+}
+
 int	get_token_length(char *str, int *j)
 {
-	int token_length;
+	int token_len;
 
-	token_length = 0;
+	token_len = 0;
 	while (str[*j] != '\0')
 	{
 		if (str[*j] == ' ')
 			skip_whitespace(str, j);
+		else if (str[*j] == '"')
+		{
+			counter_quote_len(str, j, &token_len);
+			return (token_len);
+		}
+		else if (str[*j] == '-')
+		{
+			counter_flag_len(str, j, &token_len);
+			return (token_len);
+		}
 		else if (str[*j] >= '!' && str[*j] <= '~')
 		{
-			count_str_len(str, j, &token_length);
-			return (token_length);
+			count_str_len(str, j, &token_len);
+			return (token_len);
 		}
 	}
-	return (-1);
+	if (str[*j] == '\0')
+			return (token_len);
+	else
+		return (-1);
 }
 
-void cpy_token(char *str, char *token_arr, int token_length, int start_copy)
+void cpy_token(char *str, char *token_arr, int token_len, int start_copy)
 {
 	int i;
 
 	i = 0;
-	while (i < token_length)
+	while (i < token_len)
 	{
 		token_arr[i] = str[start_copy];
 		i++;
@@ -49,16 +82,96 @@ void cpy_token(char *str, char *token_arr, int token_length, int start_copy)
 	}
 }
 
+void counter_dqoutes_len(char *str, int *j, int *token_len)
+{
+	if (str[*j] == '"' && str[*j + 1] == '"')
+	{
+		(*j) += 2;
+		return ;
+	}
+	(*token_len)++;
+	(*j)++;
+	while (str[*j] != '"' && str[*j] != '\0')
+	{
+		(*token_len)++;
+		(*j)++;
+	}
+	if (str[*j + 1] == '"')
+	{
+		(*token_len)++;
+		(*j)++;
+		return ;
+	}
+	if (str[*j + 1] != ' ')
+	{
+		while (str[*j] >= '!' && str[*j] <= '~' && str[*j] != '\0')
+		{
+			(*token_len)++;
+			(*j)++;
+		}
+	}
+	else
+	{
+		(*token_len)++;
+		(*j)++;
+	}
+}
+
+void	counter_sqoutes_len(char *str, int *j, int *token_len)
+{
+	if (str[*j] == 39 && str[*j + 1] == 39)
+	{
+		(*j) += 2;
+		return ;
+	}
+	(*token_len)++;
+	(*j)++;
+	while (str[*j] != 39 && str[*j] != '\0')
+	{
+		(*token_len)++;
+		(*j)++;
+		printf("str[%d] = %c\n", (*j), str[*j]);
+	}
+	if (str[*j + 1] == 39)
+	{
+		(*token_len)++;
+		(*j)++;
+		return ;
+	}
+	if (str[*j + 1] != ' ')
+	{
+		while (str[*j] >= '!' && str[*j] <= '~' && str[*j] != '\0')
+		{
+			(*token_len)++;
+			(*j)++;
+		}
+	}
+	else
+	{
+		(*token_len)++;
+		(*j)++;
+	}
+}
+
+void counter_quote_len(char *str, int *j, int *token_len)
+{
+	if (str[*j] == 39)
+		counter_sqoutes_len(str, j, token_len);
+	else if (str[*j] == '"')
+		counter_dqoutes_len(str, j, token_len);
+}
+
 int tokenizer(char *str)
 {
 	char	**token_arr;
 	int		token_count;
-	int		token_length;
+	int		token_len;
 	int		i;
 	int		j;
 	int		start_copy;
 
 	token_count = token_counter(str);
+	printf("token_count = %d\n", token_count);
 	token_arr = (char **) malloc(sizeof(char *) * (token_count + 1));
 	if (token_arr == NULL)
 	{
@@ -70,11 +183,13 @@ int tokenizer(char *str)
 	while (i < token_count)
 	{
 		start_copy = j;
-		token_length = get_token_length(str, &j);
-		if (token_length == -1)
+		token_len = get_token_length(str, &j);
+		if (token_len == -1)
+		{
+			printf("hier\n");
 			return (1);
-		printf("token_length = %d\n", token_length);
-		token_arr[i] = (char *) malloc(sizeof(char) * token_length + 1);
+		}
+		token_arr[i] = (char *) malloc(sizeof(char) * token_len + 1);
 		if (token_arr[i] == NULL)
 		{
 			perror("minishell: malloc failed\n");
@@ -82,11 +197,13 @@ int tokenizer(char *str)
 		}
 		skip_whitespace(str, &start_copy);
 		printf("start_cpy = %d\n", start_copy);
+		printf("toke_length = %d\n", token_len);
 		// Hier muss dann der token erstellt werden in einer funktion
-		cpy_token(str, token_arr[i], token_length, start_copy);
-		token_arr[i][token_length] = '\0';
+		cpy_token(str, token_arr[i], token_len, start_copy);
+		token_arr[i][token_len] = '\0';
 		i++;
 	}
+	token_arr[token_count] = NULL;
 	// Test ///
 	i = 0;
 	while (token_arr[i] != NULL)
@@ -95,6 +212,5 @@ int tokenizer(char *str)
 		i++;
 	}
 	// Test ///
-	token_arr[token_count] = NULL;
 	return (0);
 }
