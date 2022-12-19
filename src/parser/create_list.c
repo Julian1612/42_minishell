256432258885ccba085d576388b8b3a4d772485d
Julian Schneider <jtschneider99@icloud.com>
2022-12-19 18:54:36 +0100

    Started to change a difffrent way of the parsing, not working right now

--- src/parser/create_list.c
+++ src/parser/create_list.c
@@ -6,7 +6,7 @@
 /*   By: jschneid <jschneid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 10:07:16 by jschneid          #+#    #+#             */
-/*   Updated: 2022/12/15 16:30:25 by jschneid         ###   ########.fr       */
+/*   Updated: 2022/12/19 14:53:01 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,84 +15,88 @@
 #include <string.h>
 #include <stdlib.h>
 
-t_data	*create_list(const int nbr_cmd)
-{
-	int		i;
-	t_data	*head;
-	t_data	*new_node;
+// t_data	*create_list(const int nbr_cmd)
+// {
+// 	int		i;
+// 	t_data	*head;
+// 	t_data	*new_node;
 
-	head = create_head();
-	i = 1;
-	while (i < nbr_cmd)
-	{
-		new_node = malloc(sizeof(t_data));
-		init_node_null(new_node);
-		new_node->next = head;
-		head = new_node;
-		i++;
-	}
-	return (head);
-}
+// 	head = create_head();
+// 	i = 1;
+// 	while (i < nbr_cmd)
+// 	{
+// 		new_node = malloc(sizeof(t_data));
+// 		init_node_null(new_node);
+// 		new_node->next = head;
+// 		head = new_node;
+// 		i++;
+// 	}
+// 	return (head);
+// }
 
-t_data	*create_head(void)
-{
-	t_data	*head;
+// t_data	*create_head(void)
+// {
+// 	t_data	*head;
 
-	head = (t_data *) malloc(sizeof(t_data));
-	if (head == NULL)
-	{
-		perror("minishell: malloc failed\n");
-		return (NULL);
-	}
-	init_node_null(head);
-	return (head);
-}
+// 	head = (t_data *) malloc(sizeof(t_data));
+// 	if (head == NULL)
+// 	{
+// 		perror("minishell: malloc failed\n");
+// 		return (NULL);
+// 	}
+// 	init_node_null(head);
+// 	return (head);
+// }
 
-char	**init_cmd_line(char **token_arr, int *i)
-{
-	char	**arr;
-	int		cmd_count;
-	int		j;
+// char	**init_cmd_line(char **token_arr, int *i)
+// {
+// 	char	**arr;
+// 	int		cmd_count;
+// 	int		j;
 
-	cmd_count = count_cmd(token_arr, *i);
-	arr = (char **) malloc(sizeof(char *) * (cmd_count + 1)); // 100 ist nur ein Platzhalter
-	if (arr == NULL)
-		return (NULL);
-	j = 0;
-	while (j < cmd_count)
-	{
-		arr[j] = ft_strdup(token_arr[*i]);
-		j++;
-		(*i)++;
-	}
-	arr[j] = NULL;
-	return (arr);
-}
+// 	cmd_count = count_cmd(token_arr, *i);
+// 	arr = (char **) malloc(sizeof(char *) * (cmd_count + 1)); // 100 ist nur ein Platzhalter
+// 	if (arr == NULL)
+// 		return (NULL);
+// 	j = 0;
+// 	while (j < (cmd_count))
+// 	{
+// 		arr[j] = ft_strdup(token_arr[*i]);
+// 		j++;
+// 		(*i)++;
+// 	}
+// 	arr[j] = NULL;
+// 	return (arr);
+// }
 
-int	get_following_operator(char **token_arr, int i)
-{
-	while (token_arr[i] != NULL) // anderen opperatoren einfügen >>, <<, ||
-	{
-		if (token_arr[i][0] == '|')
-			return (PIPE);
-		i++;
-	}
-	return (CMD);
-}
+// int	get_operator(char **token_arr, int i)
+// {
+// 	while (token_arr[i] != NULL) //>>, <<
+// 	{
+// 		if (token_arr[i][0] == '|')
+// 			return (PIPE);
+// 		else if (token_arr[i][0] == '<')
+// 			return (IN);
+// 		else if (token_arr[i][0] == '>')
+// 			return (OUT);
+// 		i++;
+// 	}
+// 	return (CMD);
+// }
 
-void	init_list(t_data *head, char **token_arr)
-{
-	int		i;
-	t_data	*tmp;
+// void	init_list(t_data *head, char **token_arr)
+// {
+// 	int		i;
+// 	t_data	*tmp;
 
-	i = 0;
-	tmp = head;
-	while (token_arr[i] != NULL && tmp != NULL)
-	{
-		tmp->cmd_name = ft_strdup(token_arr[i]);
-		tmp->cmd_line = init_cmd_line(token_arr, &i);
-		tmp->operator = get_following_operator(token_arr, i);
-		tmp = tmp->next;
-		i++;
-	}
-}
+// 	i = 0;
+// 	tmp = head;
+// 	while (token_arr[i] != NULL && tmp != NULL)
+// 	{
+// 		tmp->cmd_name = ft_strdup(token_arr[i]);
+// 		tmp->operator = get_operator(token_arr, i);
+// 		tmp->cmd_line = init_cmd_line(token_arr, &i);
+// 		tmp = tmp->next;
+// 		i++;
+// 	}
+// }
