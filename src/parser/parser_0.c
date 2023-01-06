92a36dc56ee91ac4de2355c0cc2200bf7b5a7180
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-06 16:47:44 +0100

    fixed merge issues

--- src/parser/parser_0.c
+++ src/parser/parser_0.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "libft.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
