90d596dd687d4e6aadebae4e01eedef55a292767
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-09 19:27:01 +0100

    norm fix

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 11:50:03 by jschneid          #+#    #+#             */
-/*   Updated: 2023/01/09 19:19:56 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/01/09 19:26:32 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,37 @@
 #include <string.h>
 #include <stdlib.h>
 
-void	list_test(t_data *cmd_list)
-{
-	t_data	*cur;
-	cur = cmd_list;
-	int g = 1;
-	int y = 0;
-	while (cur != NULL)
-	{
-		printf("\e[37m----------------------\n");
-		printf("\e[106m----------%d-----------\e[49m\n", g);
-		printf("\e[37m----------------------\n");
-		printf("\e[32mcmd: %s\n", cur->cmd_name);
-		printf("\e[37m~~~~~~~~~~~~~~~~~~~~~~\n");
-		if (cur->cmd_line != NULL)
-		{
-			while (cur->cmd_line[y] != NULL)
-			{
-				printf("\e[34m%d.%d. cmd_line: %s\n", g, y, cur->cmd_line[y]);
-				y++;
-			}
-			y = 0;
+// void	list_test(t_data *cmd_list)
+// {
+// 	t_data	*cur;
+// 	cur = cmd_list;
+// 	int g = 1;
+// 	int y = 0;
+// 	while (cur != NULL)
+// 	{
+// 		printf("\e[37m----------------------\n");
+// 		printf("\e[106m----------%d-----------\e[49m\n", g);
+// 		printf("\e[37m----------------------\n");
+// 		printf("\e[32mcmd: %s\n", cur->cmd_name);
+// 		printf("\e[37m~~~~~~~~~~~~~~~~~~~~~~\n");
+// 		if (cur->cmd_line != NULL)
+// 		{
+// 			while (cur->cmd_line[y] != NULL)
+// 			{
+// 				printf("\e[34m%d.%d. cmd_line: %s\n", g, y, cur->cmd_line[y]);
+// 				y++;
+// 			}
+// 			y = 0;
 
-		}
-		printf("\e[37m~~~~~~~~~~~~~~~~~~~~~~\n");
-		printf("\e[95moperator: %d\033[0m\n", cur->operator);
-		printf("\e[37m~~~~~~~~~~~~~~~~~~~~~~\n");
-		printf("\e[95mredir: %d\033[0m\n", cur->redir);
-		cur = cur->next;
-		g++;
-	}
-}
+// 		}
+// 		printf("\e[37m~~~~~~~~~~~~~~~~~~~~~~\n");
+// 		printf("\e[95moperator: %d\033[0m\n", cur->operator);
+// 		printf("\e[37m~~~~~~~~~~~~~~~~~~~~~~\n");
+// 		printf("\e[95mredir: %d\033[0m\n", cur->redir);
+// 		cur = cur->next;
+// 		g++;
+// 	}
+// }
 
 t_data	*parser(char **token_arr)
 {
